50c3b2fc79c225fe1fe91c7b96df4777dbe30fb8
Lucas Rodrigues Dias <86005352+lucasrd2k@users.noreply.github.com>
2022-10-15 13:01:08 -0300

    commit inicial

---
+++ models/Book.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const Book = db.define('material', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    arquivo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//Criar a tabela
+Book.sync();
+//Verificar se há alguma diferença na tabela, realiza a alteração
+// Book.sync({ force: true })
+
+module.exports = Book;
